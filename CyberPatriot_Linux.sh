dc817069af3031d1adddc7f89b1811f839ecda09
siraaj-git <sqadir@smail.pembinatrails.ca>
2024-10-30 13:51:05 -0500

    Added function to check for unnecessary files

--- CyberPatriot_Linux.sh
+++ CyberPatriot_Linux.sh
@@ -7,3 +7,11 @@ getAllUsers() {
         fi
     done
 }
+
+checkUnnecessaryFiles() {
+    awk -F: '$3 >= 1000 {print $1, $6}' /etc/passwd | while read -r user home_dir; do
+        echo "Checking for unnecessary files in $user's home directory: $home_dir"
+        
+        find "$home_dir" -type f \( -name "*.mp3" -o -name "*.wav" -o -name "*.png" -o -name "*.jpg" \) ! -path "*/.*" -print
+    done
+}
